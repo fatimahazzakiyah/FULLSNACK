38c6a67d9d5c6d148a6920b844951b6039fcdc81
fatimahazzakiyah <fatimahazzakiyah6@gmail.com>
2026-04-14 11:35:59 +0700

    simpan sementara sebelum pull

--- Downloads/FULLSNACK-main/FULLSNACK-main/backend/db.js
+++ Downloads/FULLSNACK-main/FULLSNACK-main/backend/db.js
@@ -1,21 +1,20 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "fullsnack_db"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "fullsnack_db",
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Database error:", err);
-    } else {
-        console.log("Database connected");
-    }
+  if (err) {
+    console.log("Database error:", err);
+  } else {
+    console.log("Database connected");
+  }
 });
 
 module.exports = db;
-
 
 //update by arra//
--- Downloads/FULLSNACK-main/FULLSNACK-main/backend/routes/auth.js
+++ Downloads/FULLSNACK-main/FULLSNACK-main/backend/routes/auth.js
@@ -4,38 +4,39 @@ const db = require("../db");
 
 // REGISTER
 router.post("/register", (req, res) => {
-    const { name, email, password, role } = req.body;
+  const { name, email, password, role } = req.body;
 
-    const sql = "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
+  const sql =
+    "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
 
-    db.query(sql, [name, email, password, role || "user"], (err, result) => {
-        if (err) {
-            return res.json({ message: "Register gagal" });
-        }
-        res.json({ message: "Register berhasil" });
-    });
+  db.query(sql, [name, email, password, role || "user"], (err, result) => {
+    if (err) {
+      return res.json({ message: "Register gagal" });
+    }
+    res.json({ message: "Register berhasil" });
+  });
 });
 
 // LOGIN
 router.post("/login", (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const sql = "SELECT * FROM users WHERE email=? AND password=?";
+  const sql = "SELECT * FROM users WHERE email=? AND password=?";
 
-    db.query(sql, [email, password], (err, results) => {
-        if (err) return res.json({ message: "Server error" });
+  db.query(sql, [email, password], (err, results) => {
+    if (err) return res.json({ message: "Server error" });
 
-        if (results.length > 0) {
-            const user = results[0];
+    if (results.length > 0) {
+      const user = results[0];
 
-            res.json({
-                message: "Login berhasil",
-                role: user.role
-            });
-        } else {
-            res.json({ message: "Login gagal" });
-        }
-    });
+      res.json({
+        message: "Login berhasil",
+        role: user.role,
+      });
+    } else {
+      res.json({ message: "Login gagal" });
+    }
+  });
 });
 
 module.exports = router;
