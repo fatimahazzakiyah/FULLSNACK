e38eac342ad1cc040b1ad3be939c6b23c1543a4a
revani18 <revani1811@gmail.com>
2026-05-13 13:40:52 +0700

    revani: validasi login

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -7,7 +7,7 @@ const AuthController = {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-      return res.status(400).json({ message: "Semua kolom wajib diisi! ❌" });
+      return res.status(400).json({ message: "Semua kolom wajib diisi!" });
     }
 
     // Cek email
@@ -16,10 +16,10 @@ const AuthController = {
       if (err) return res.status(500).json({ error: err.message });
 
       if (results.length > 0) {
-        return res.status(400).json({ message: "Email sudah terdaftar! 📧" });
+        return res.status(400).json({ message: "Email sudah terdaftar!" });
       }
 
-      // 🔥 TAMBAHAN: HASH PASSWORD
+      // HASH PASSWORD
       const hashedPassword = await bcrypt.hash(password, 10);
 
       const insertQuery =
@@ -27,12 +27,12 @@ const AuthController = {
 
       db.query(
         insertQuery,
-        [username, email, hashedPassword, "user"], // ✅ pakai hash
+        [username, email, hashedPassword, "user"], // pakai hash
         (err, result) => {
           if (err) return res.status(500).json({ error: err.message });
 
           res.status(201).json({
-            message: "Registrasi berhasil dan tersimpan di database! ✨",
+            message: "Registrasi berhasil dan tersimpan di database!",
             userId: result.insertId,
           });
         }
@@ -44,30 +44,38 @@ const AuthController = {
   login: (req, res) => {
     const { email, password } = req.body;
 
-    // 🔥 UBAH: ambil berdasarkan email saja
+    // VALIDASI INPUT KOSONG
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email dan password wajib diisi!",
+      });
+    }
+
+    // ambil berdasarkan email saja
     const query = "SELECT * FROM users WHERE email = ?";
+
     db.query(query, [email], async (err, results) => {
       if (err) return res.status(500).json({ error: err.message });
 
       if (results.length === 0) {
         return res
           .status(401)
-          .json({ message: "Email atau Password salah! ❌" });
+          .json({ message: "Email atau Password salah! " });
       }
 
       const user = results[0];
 
-      // 🔥 TAMBAHAN: compare bcrypt
+      // compare bcrypt
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
         return res
           .status(401)
-          .json({ message: "Email atau Password salah! ❌" });
+          .json({ message: "Email atau Password salah!" });
       }
 
       res.status(200).json({
-        message: `Selamat datang kembali, ${user.username}! 👋`,
+        message: `Selamat datang kembali, ${user.username}! `,
         user: {
           id: user.id,
           username: user.username,
